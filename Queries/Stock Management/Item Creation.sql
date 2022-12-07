2c87a16c08d6d75ab0547b3f5b5ec7a9a662f1b0
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2022-12-07 10:18:27 +0100

    Update Item Creation.sql
    Changed the given example to one that suits the enterprise's theme

--- Queries/Stock Management/Item Creation.sql
+++ Queries/Stock Management/Item Creation.sql
@@ -1,4 +1,4 @@
-INSERT INTO items VALUES ('B230F', 'Volvo 2.3 Redblock Injection 114cv', 5, 2, 1000, 20, 1);
+INSERT INTO items VALUES ('ABX00003', 'Arduino Zero', 5, 2, 38, 20, 1);
 -- The first value is the item's reference
 -- The second value is the name of the item
 -- The third value is the available quantity of items
